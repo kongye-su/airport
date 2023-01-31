e1230a437ef3d3d129fc5332862112ccba24a7a8
kongye-su <1752604028@qq.com>
2023-01-31 22:09:44 +0800

    add fastlink

--- airport.py
+++ airport.py
@@ -12,7 +12,8 @@ class Airport:
 
     def start(self):
         self.speed_cat()
-        self.ssru8()
+        # self.ssru8()
+        # self.fastlink()
 
     def get_json(self, url, params=None):
         return self.sess.get(url, params=params).json()
@@ -46,6 +47,7 @@ class Airport:
         return name
 
     def speed_cat(self):
+        # 3天5G流量
         register_url = "https://aff03.speedcat-aff02.com/auth/register"
         data = {"email": self.email,
                 "name": self.name,
@@ -73,10 +75,12 @@ class Airport:
                 res = self.get_html(user_url)
                 result = re.search(r"apiv2/([a-z1-9]+)\?", res.text)
                 key = result[1]
+                print("-------------speedcat-------------")
                 print("ShadowSocket订阅： https://apiv2.pptiok2020.com/apiv2/%s?sub=2&extend=1" % key)
                 print("Clash订阅： https://apiv2.pptiok2020.com/apiv2/%s?clash=1&extend=11" % key)
 
     def ssru8(self):
+        # 1天1G+流量
         register_url = "https://ssru8.icu/auth/register"
         data = {"email": self.email,
                 "name": self.name,
@@ -87,13 +91,45 @@ class Airport:
                 "code": "0"}
         res_json = self.post_json(register_url, data=data)
         if res_json['ret'] == 1:
-            self.post_json("https://ssru8.icu/user/checkin")
+            res_json = self.post_json("https://ssru8.icu/user/checkin")
             user_url = "https://ssru8.icu/user"
             res = self.get_html(user_url)
             result = re.search(r"link/([a-zA-Z1-9]+)\?", res.text)
             key = result[1]
+            print("-------------ssru8-------------")
             print("ShadowSocket订阅：https://lting.cyou/link/%s?sub=3" % key)
             print("Clash订阅： https://lting.cyou/link/%s?clash=1" % key)
+
+    def fastlink(self):
+        # 3天5G流量
+        register_url = "https://v1.fastlink-aff02.com/auth/register"
+        data = {"email": self.email,
+                "name": self.name,
+                "passwd": self.password,
+                "repasswd": self.password,
+                "code": "0"}
+        res_json = self.post_json(register_url, data=data)
+        if res_json['ret'] == 1:
+            login_url = "https://v1.fastlink-aff02.com/auth/login"
+            data = {"email": self.email,
+                    "passwd": self.password,
+                    "code": ""}
+            res_json = self.post_json(login_url, data)
+            buy_url = "https://v1.fastlink-aff02.com/user/buy"
+            data = {
+                "coupon": "599_f3cz8bzm",
+                "shop": "3",
+                "autorenew": "0",
+                "disableothers": "1"
+            }
+            res_json = self.post_json(buy_url, data)
+            user_url = "https://v1.fastlink-aff02.com/user"
+            res = self.get_html(user_url)
+            result = re.search(r"api_version2/([a-z1-9]+)\?", res.text)
+            key = result[1]
+            print("-------------FASTLINK-------------")
+            print("ShadowSocket订阅： https://apiv2.lipulai.com/api_version2/%s?sub=3&extend=1" % key)
+            print("Clash订阅： https://apiv2.lipulai.com/api_version2/%s?clash=1&extend=1" % key)
 
 
 if __name__ == "__main__":
