26a435a7c2c21a19ba5d0f8d1b005470eb41d5aa
kongye-su <1752604028@qq.com>
2023-01-31 21:43:03 +0800

    init

---
+++ airport.py
@@ -0,0 +1,100 @@
+import re
+import random
+import requests
+
+
+class Airport:
+    def __init__(self):
+        self.sess = requests.Session()
+        self.email = self.random_email()
+        self.password = self.random_password()
+        self.name = self.random_name()
+
+    def start(self):
+        # self.speed_cat()
+        self.ssru8()
+
+    def get_json(self, url, params=None):
+        return self.sess.get(url, params=params).json()
+
+    def post_json(self, url, data=None):
+        return self.sess.post(url, data=data).json()
+
+    def get_html(self, url, params=None):
+        return self.sess.get(url, params=params)
+
+    def random_email(self):
+        length = random.randint(5, 20)
+        email_prefixs = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPWRSTUVWXYZ"
+        email_prefix = "".join([random.choice(email_prefixs) for _ in range(length)])
+        email_suffixs = ["@163.com", "@qq.com", "@gmail.com", "@outlook.com",
+                         "@yahoo.com", "@hotmail.com", "@126.com",
+                         "@foxmail.com"]
+        email_suffix = random.choice(email_suffixs)
+        return email_prefix + email_suffix
+
+    def random_password(self):
+        length = random.randint(8, 15)
+        passwords = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPWRSTUVWXYZ.+-*/~`@!#$%^&()_=':;<>,?/"
+        password = "".join([random.choice(passwords) for _ in range(length)])
+        return password
+
+    def random_name(self):
+        length = random.randint(5, 15)
+        names = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPWRSTUVWXYZ"
+        name = "".join([random.choice(names) for _ in range(length)])
+        return name
+
+    def speed_cat(self):
+        register_url = "https://aff03.speedcat-aff02.com/auth/register"
+        data = {"email": self.email,
+                "name": self.name,
+                "passwd": self.password,
+                "repasswd": self.password,
+                "code": "speedcat"}
+        res_json = self.post_json(register_url, data=data)
+        if res_json['ret'] == 1:
+            login_url = "https://aff03.speedcat-aff02.com/auth/login"
+            data = {"email": self.email,
+                    "passwd": self.password,
+                    "code": "",
+                    "remember_me": "on"}
+            res_json = self.post_json(login_url, data)
+            if res_json["ret"] == 1:
+                buy_url = "https://aff03.speedcat-aff02.com/user/buy"
+                data = {
+                    "coupon": "free",
+                    "shop": "16",
+                    "autorenew": "0",
+                    "disableothers": "1"
+                }
+                res_json = self.post_json(buy_url, data)
+                user_url = "https://aff03.speedcat-aff02.com/user"
+                res = self.get_html(user_url)
+                result = re.search(r"apiv2/([a-z1-9]+)\?", res.text)
+                key = result[1]
+                print("ShadowSocket订阅： https://apiv2.pptiok2020.com/apiv2/%s?sub=2&extend=1" % key)
+                print("Clash订阅： https://apiv2.pptiok2020.com/apiv2/%s?clash=1&extend=11" % key)
+
+    def ssru8(self):
+        register_url = "https://ssru8.icu/auth/register"
+        data = {"email": self.email,
+                "name": self.name,
+                "passwd": self.password,
+                "repasswd": self.password,
+                "wechat": "",
+                "imtype": "",
+                "code": "0"}
+        res_json = self.post_json(register_url, data=data)
+        if res_json['ret'] == 1:
+            user_url = "https://ssru8.icu/user"
+            res = self.get_html(user_url)
+            result = re.search(r"link/([a-zA-Z1-9]+)\?", res.text)
+            key = result[1]
+            print("ShadowSocket订阅：https://lting.cyou/link/%s?sub=3" % key)
+            print("Clash订阅： https://lting.cyou/link/%s?clash=1" % key)
+
+
+if __name__ == "__main__":
+    a = Airport()
+    a.start()
---
+++ 机场.bat
@@ -0,0 +1,2 @@
+conda activate spider & python airport.py
+pause
